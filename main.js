2498b2b97c9ec06d6d5f293e5f99fec261b32139
SlendyEscobarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 17:22:53 -0500

    JAVA_07

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-let trabas= Number(prompt("Ingresar el numero de trabajadores:"));
-    for (let i=1; i<=trabas; i++){
-        let tiem= Number(prompt("Ingresar el tiempo que lleva en la empresa:"));
-        
-        if (tiem>0 && tiem<=5){
-            alert('Usted cuenta con un aumento de 100 pesos');
-        } else if(tiem>5 && tiem<=10){
-            alert("Usted cuenta con un aumento de 250 pesos");
-        } else if (tiem>10 && tiem>=20){
-            alert('Udted cuenta con un aumento de 400 pesos');
-        } else{
-            alert('Usted tiene un aumento de 550 pesos');
-        }
+let angu= document.querySelector("#angu").value;
+let serie= document.querySelector("#serie").value;
+let res= 0, num= 3, facto= 1;
+
+for (let i=1; i<=serie; i++){
+    for (let j=1; j<=num; j++){
+        facto *= j;
     }
+    if (i%2==0){
+        res+= (angu*num)/facto;
+        num+= 2;
+    } else{
+        res-= (angu*num)/facto;
+        num+= 2;
+    }
+    alert('El seno del angulo, ${angu} es, ${res}');
+}
