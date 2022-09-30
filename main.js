a651683379649c2693f827ab992938750c992fc6
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 12:32:33 -0500

    JAVA_03

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-let num= 3;
+let  estu= document.querySelector("#estu").value;
+let notaFin= 0,menor=10, mayor=0;
 
-for (let i=1; i<=24;i++){
-    if (i%2==0){
-        num+=4;
-        alert(num);
-    }else{
-        num-=2;
-        alert(num);
+for (let i=1; estu>=i; i++){
+    let cali= parseInt(prompt("Ingresar la calificacion de la cafeteria:"));
+    notaFin= notaFin+cali;
+
+    if (cali>mayor){
+        mayor= cali;
+    }else if (cali<menor){
+        menor= cali;
     }
+    let pro= notaFin/estu;
+    alert('La calificacion mayor es de:' +mayor);
+    alert('La calificacion menor es de:' +menor);
+
+    alert('El promedio es de:' +pro);
 }
