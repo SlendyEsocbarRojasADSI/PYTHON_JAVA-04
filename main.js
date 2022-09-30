3ff27e1b8fe0a1f595dcfe94efa9af474efa42a2
SlendyEscobarRojasADSI <sleppymorningxd@gmail.com>
2022-09-30 17:09:45 -0500

    JAVA_06

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-let vent= Number(prompt("Ingresar el numero de ventas:"));
-let ventMen= 0;
-let ventMedi= 0;
-let ventMayo= 0;
-
-for (let i; i<=vent; i++){
-    let preci= Number(prompt("Digitar el precio de la venta:"));
-
-    if (preci<500){
-        ventMen++
-    } else if (preci>500 && preci<100){
-        ventMedi++
-    } else{
-        ventMayo++
+let trabas= Number(prompt("Ingresar el numero de trabajadores:"));
+    for (let i=1; i<=trabas; i++){
+        let tiem= Number(prompt("Ingresar el tiempo que lleva en la empresa:"));
+        
+        if (tiem>0 && tiem<=5){
+            alert('Usted cuenta con un aumento de 100 pesos');
+        } else if(tiem>5 && tiem<=10){
+            alert("Usted cuenta con un aumento de 250 pesos");
+        } else if (tiem>10 && tiem>=20){
+            alert('Udted cuenta con un aumento de 400 pesos');
+        } else{
+            alert('Usted tiene un aumento de 550 pesos');
+        }
     }
-    alert('Las ventas menores a 500 fueron:' +ventMen);
-    alert('Las ventas medianos a 1000 pero mayores a 500 fueron:' +ventMedi);
-    alert('Las ventas mayores a 1000 fueron:' +ventMayo);
-}
